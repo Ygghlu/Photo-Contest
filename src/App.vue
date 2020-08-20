c2bde23ee693d80aeec7c0e2303f829b8298aa36
Ygghlu <36296646+Ygghlu@users.noreply.github.com>
2020-08-20 23:29:27 +0700

    finish

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,123 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="w-100 h-100 p-3 mx-auto flex-column">
+      <header class="masthead mb-5">
+        <div class="inner">
+          <h3 class="masthead-brand">Photo Contest</h3>
+          <nav class="nav nav-masthead justify-content-center">
+            <a class="nav-link active" href="#">Home</a>
+            <a class="nav-link" href="#carousel">Carousal</a>
+            <a class="nav-link" href="#vote">Vote</a>
+          </nav>
+        </div>
+      </header>
+      <div class="col-7 mx-auto">
+        <carousel
+          id="carousel"
+          :perPage="1"
+          :navigationEnabled="true"
+          :paginationColor="pink"
+          :centerMode="true"
+          :autoplay="true"
+          :loop="true"
+        >
+          <slide>
+            <a href="#con1">
+              <img :src="pic[0].src" class="img-fluid" />
+            </a>
+
+            <br />
+            <br />Contestant No. 1
+          </slide>
+          <slide>
+            <a href="#con2">
+              <img :src="pic[1].src" class="img-fluid" />
+            </a>
+
+            <br />
+            <br />Contestant no.2
+          </slide>
+          <slide>
+            <a href="#con3">
+              <img :src="pic[2].src" class="img-fluid" />
+            </a>
+
+            <br />
+            <br />Contestant no.3
+          </slide>
+        </carousel>
+      </div>
+    </div>
+
+    <div id="vote" class="container-fluid">
+      <div class="col mx-auto mb-3">
+        <h1>Voting</h1>
+      </div>
+
+      <div id="con1" class="row align-items-center mb-5">
+        <div class="col-md-7">
+          <img :src="pic[0].src" class="img-fluid" />
+          <br />
+          <br />Contestant No. 1
+        </div>
+        <div class="col-md-5">
+          <score />
+        </div>
+      </div>
+      <div id="con2" class="row align-items-center mb-5">
+        <div class="col-md-7">
+          <img :src="pic[1].src" class="img-fluid" />
+          <br />
+          <br />Contestant No. 2
+        </div>
+        <div class="col-md-5">
+          <score />
+        </div>
+      </div>
+      <div id="con3" class="row align-items-center mb-5">
+        <div class="col-md-7">
+          <img :src="pic[2].src" class="img-fluid" />
+          <br />
+          <br />Contestant No. 3
+        </div>
+        <div class="col-md-5">
+          <score />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "./assets/CSS/cover.css";
+import Vue from "vue";
+import VueCarousel from "vue-carousel";
+Vue.use(VueCarousel);
+import score from "./components/score";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { score },
+  data: function () {
+    return {
+      pic: [
+        {
+          src:
+            "https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_960_720.jpg",
+        },
+        {
+          src:
+            "https://cdn.pixabay.com/photo/2017/02/08/17/24/butterfly-2049567_960_720.jpg",
+        },
+        {
+          src:
+            "https://cdn.pixabay.com/photo/2016/08/11/23/48/italy-1587287_960_720.jpg",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/score.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <div class="row ">
+      <div class="col-2">
+        <button @click="likefunction" class="btn">
+          <img class="img-fluid" src="../../public/image/like.png" alt="like" />
+        </button>
+      </div>
+      <div class="col-2">
+        <button @click="lovefunction" class="btn">
+          <img class="img-fluid" src="../../public/image/love.png" alt="love" />
+        </button>
+      </div>
+      <div class="col-2">
+        <button @click="hahafunction" class="btn">
+          <img class="img-fluid" src="../../public/image/haha.png" alt="haha" />
+        </button>
+      </div>
+      <div class="col-2">
+        <button @click="wowfunction" class="btn">
+          <img class="img-fluid" src="../../public/image/wow.png" alt="wow" />
+        </button>
+      </div>
+      <div class="col-2">
+        <button @click="sadfunction" class="btn">
+          <img class="img-fluid" src="../../public/image/sad.png" alt="sad" />
+        </button>
+      </div>
+      <div class="col-2">
+        <button @click="angryfunction" class="btn">
+          <img
+            class="img-fluid"
+            src="../../public/image/angry.png"
+            alt="angry"
+          />
+        </button>
+      </div>
+    </div>
+    <br />
+    <p>This photo score : {{ counter }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      counter: 0,
+    };
+  },
+
+  methods: {
+    likefunction: function() {
+      this.counter += 1;
+    },
+    lovefunction: function() {
+      this.counter += 10;
+    },
+    hahafunction: function() {
+      this.counter += 5;
+    },
+    wowfunction: function() {
+      this.counter += 20;
+    },
+    sadfunction: function() {
+      this.counter -= 5;
+    },
+    angryfunction: function() {
+      this.counter -= 50;
+    },
+  },
+};
+</script>
+
+<style></style>
