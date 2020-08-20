bf4aa0c30bf43188a6a49159d1a45912b74dbbbb
Ygghlu <36296646+Ygghlu@users.noreply.github.com>
2020-08-21 00:16:30 +0700

    add score clarify

--- src/components/score.vue
+++ src/components/score.vue
@@ -4,26 +4,31 @@
       <div class="col-2">
         <button @click="likefunction" class="btn">
           <img class="img-fluid" src="../../public/image/like.png" alt="like" />
+          <p class="text-info">+1</p>
         </button>
       </div>
       <div class="col-2">
         <button @click="lovefunction" class="btn">
           <img class="img-fluid" src="../../public/image/love.png" alt="love" />
+          <p class="text-info">+10</p>
         </button>
       </div>
       <div class="col-2">
         <button @click="hahafunction" class="btn">
           <img class="img-fluid" src="../../public/image/haha.png" alt="haha" />
+          <p class="text-info">+5</p>
         </button>
       </div>
       <div class="col-2">
         <button @click="wowfunction" class="btn">
           <img class="img-fluid" src="../../public/image/wow.png" alt="wow" />
+          <p class="text-info">+20</p>
         </button>
       </div>
       <div class="col-2">
         <button @click="sadfunction" class="btn">
           <img class="img-fluid" src="../../public/image/sad.png" alt="sad" />
+          <p class="text-danger">-5</p>
         </button>
       </div>
       <div class="col-2">
@@ -33,6 +38,7 @@
             src="../../public/image/angry.png"
             alt="angry"
           />
+          <p class="text-danger">-50</p>
         </button>
       </div>
     </div>
